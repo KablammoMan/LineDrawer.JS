783acdc4f60792617d722a6d6e670e280e9e6eae
KablammoMan <codernaffilio@gmail.com>
2023-05-22 14:43:41 +1000

    Update draw.js

---
+++ draw.js
@@ -0,0 +1,23 @@
+// Object Class
+class obj {
+    constructor(radius, speed) {
+        this.radius = radius;
+        this.speed = speed;
+    }
+}
+
+const canvas = document.createElement("canvas");
+
+window.addEventListener("load", e => {
+    let all_elems = document.querySelectorAll("*");
+    for (let elem of all_elems) {
+        elem.style.margin = "0";
+        elem.style.padding = "0";
+    }
+    document.body.appendChild(canvas);
+});
+
+window.addEventListener("resize", e => {
+    canvas.style.width = window.innerWidth;
+    canvas.style.height = window.innerHeight;
+})

7646cf76900966aa2d7f569ec7d6a97571cbf16e
KablammoMan <codernaffilio@gmail.com>
2023-05-22 15:13:02 +1000

    Update draw.js

--- draw.js
+++ draw.js
@@ -3,29 +3,33 @@ class obj {
     constructor(radius, speed) {
         this.radius = radius;
         this.speed = speed;
+        this.deg = 0;
+    }
+    update_pos() {
+        this.rad = this.deg * Math.PI / 180
+        this.x = Math.cos(this.rad);
+        this.y = Math.sin(this.rad);
     }
 }
 
 class area {
     constructor() {
         this.canvas = document.createElement("canvas");
-        this.canvas.style.backgroundColor = "#aaa";
+        this.canvas.width = window.innerWidth;
+        this.canvas.height = window.innerHeight;
         document.body.insertBefore(this.canvas, document.body.firstChild);
+        this.ctx = this.canvas.getContext("2d");
     }
     update_size() {
-        this.canvas.style.width = window.innerWidth.toString() + "px";
-        this.canvas.style.height = window.innerHeight.toString() + "px";
+        this.canvas.width = window.innerWidth;
+        this.canvas.height = window.innerHeight;
     }
 }
 
 const draw_area = new area();
 
 window.addEventListener("load", e => {
-    let all_elems = document.querySelectorAll("*");
-    for (let elem of all_elems) {
-        elem.style.margin = "0";
-        elem.style.padding = "0";
-    }
+    draw_area.start();
     draw_area.update_size();
 });
 
